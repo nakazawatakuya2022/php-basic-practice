5f7b4261e3087d3584a7071616ebe35ce9f8a7b9
Takuya Nakazawa <nakazawata98@gmail.com>
2025-11-12 14:03:10 +0900

    feat: add Day2 DB connection and item list

--- day2_crud/db.php
+++ day2_crud/db.php
@@ -3,7 +3,7 @@
 <?php
 // LAMP学習用: 学校サーバ(192.168.0.4)のMariaDBにTCP接続
 //$dsn = 'mysql:host=192.168.0.4;port=3306;dbname=app;charset=utf8mb4';
-$dsn = 'mysql:host=192.168.0.4;port=3306;dbname=app;charset=utf8mb4';
+$dsn = 'mysql:host=127.0.0.1;port=3306;dbname=app;charset=utf8mb4';
 $user = 'web2511';   // 学校の設定に合わせて変更
 $pass = 'web2511';       // パスワード設定があれば入力
 try {
